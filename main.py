38aa77ec9d8bd6ff2fac5b8360aaf1b70564b994
Alejo1201 <equinterogiraldo12@gmail.com>
2023-04-06 16:20:57 -0500

    Change

--- main.py
+++ main.py
@@ -13,10 +13,17 @@ class Msg(BaseModel):
     msg: str
     secret: str
 
-@app.get("/")
+# url local http://127.0.0.1:8000/
 
+@app.get("/")
 async def root():
-    return "<h1>Hola mauro</h1>"
+    return "<h1>Hola Mundo</h1>"
+
+# url local http://127.0.0.1:8000/url
+
+@app.get("/url")
+async def url():
+    return {"url_curso": "https://mouredev.com/python"}
 
 
 @app.get("/homepage")
