b83f06a36dd6cf4643d8989d9ba1834b9c2d3c18
Alejo1201 <equinterogiraldo12@gmail.com>
2023-04-06 22:05:57 -0500

    Change

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ list = []
 
 lista = lleverInfo(list)
 
-datos_list = [Dato(dato1 = list[0], dato2 = list[1], dato3 = list[2])]
+datos_list = [Dato(dato1 = lista[0], dato2 = lista[1], dato3 = lista    [2])]
 
 @app.get("/datos")
 async def root():
